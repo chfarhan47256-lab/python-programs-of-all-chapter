6f1a5f5c3e8aac40c73568f329f5466484bece25
M Farhan Ali <ch.farhan47256@gmail.com>
2026-07-13 12:17:31 +0500

    program 9.2 find large number using function

---
+++ chapter 9/chapter program/program 9.2.py
@@ -0,0 +1,8 @@
+def max(a,b):
+    if a>b:
+        print("1st number",a,"is large number")
+    else:
+        print("2nd number",b,"is large number")
+num1 = int(input("Enter 1st number:"))
+num2  =int(input("Enter 2nd number:"))
+max(num1,num2)
